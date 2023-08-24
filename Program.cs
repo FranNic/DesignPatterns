70cd1a44cf74253d2216c1935bc8c7b50339c4f9
Francisco Nicosia <nicosia.francisco@gmail.com>
2023-08-24 14:23:09 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Builder;
+using Builder.FluentGeneric;
+using Builder.Stepwise;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/// <summary>
+/// Basic builder pattern. No order required. Cannot chain methods. One can add a Director class to enforce order and encapsulate business logic.
+/// </summary>
+/// 
+#region Builder
+var carBuilder = new CarBuilder();
+carBuilder.BuildBody();
+carBuilder.BuildChassis();
+carBuilder.BuildElectronics();
+carBuilder.BuildEngine();
+carBuilder.BuildWindows();
+var car = carBuilder.GetCar();
+Console.WriteLine(car);
+#endregion
+
+
+/// <summary>
+/// Basic Fluent builder pattern. No order required. Can chain methods.
+/// </summary>
+/// 
+#region FluentBuilder
+var fluentCarBuilder = new FluentCarBuilder();
+fluentCarBuilder.BuildBody().BuildEngine().BuildWindows().BuildElectronics().BuildChassis();
+var fluentCar = fluentCarBuilder.GetCar();
+Console.WriteLine(fluentCar);
+#endregion
+
+/// <summary>
+/// Basic Generic Fluent builder pattern. No order required. Can chain methods.
+/// </summary>
+/// 
+#region GenericFluentBuilder
+var carBuilderGenericDirector = GenericCarBuilderDirector.NewCar.AddBody().AddWindows();
+#endregion
+
+
+/// <summary>
+/// Stepwise builder pattern. Order required. Must chain methods.
+/// </summary>
+/// 
+#region StepwiseBuilder
+var stepwiseCarBuilder = StepwiseCarBuilder.Create().BuildChassis().BuildEngine().BuildElectronics().BuildCarBody().Build();
+Console.WriteLine(stepwiseCarBuilder);
+#endregion
+
